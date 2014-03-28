ecfb412924ae01de7ace002c4742c7bf335d7525
abhijeetka <abhijeet.kamble619@gmail.com>
2014-03-28 19:40:12 +0530

    updated controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,30 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var dependencyControllers = angular.module('dependencyControllers',[]);
 
-  }])
-  .controller('MyCtrl2', [function() {
+dependencyControllers.controller('controller1',['$scope','$location','$firebase',function($scope,$location,$firebase){
+	
+	var currentRef = new Firebase('https://djwadichat.firebaseio.com');
+	var loginStatus = false;
+	$scope.auth = new FirebaseSimpleLogin(currentRef,function(error,user,$location){
+		alert('called firebase simple login');
+		if(error){
+			console.log('Error not able to login');
+		}
+		else if(user){			
+			console.log('userid: '+ user.id+ "  "+ "provider: "+ user.provider);	
+			loginStatus = true;		
+		}else{
+			console.log('user is logged out');
+		}
 
-  }]);
+});
+	$scope.authenticate =function($auth){
+  		console.log('abhijeet');
+  		$scope.auth.login('google',{debug:'true'});
+  		alert('after auth function');
+
+	}
+}]);
+
