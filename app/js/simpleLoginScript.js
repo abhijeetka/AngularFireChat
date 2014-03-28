5c96896d648dbd7ade08c9fbec014107447fcb21
abhijeetka <abhijeet.kamble619@gmail.com>
2014-03-28 19:41:35 +0530

    upated

--- app/js/simpleLoginScript.js
+++ app/js/simpleLoginScript.js
@@ -1,20 +1,7 @@
-var currentRef = new Firebase('https://djwadichat.firebaseio.com');
 
 
-$( "#Submit" ).on('click',function() {
-  console.log('abhijeet');
 
-	var auth = new FirebaseSimpleLogin(currentRef,function(error,user){
-		if(error){
-			console.log('Error not able to login');
-		}
-		else if(user){
-			console.log('userid: '+ user.id+ "  "+ "provider: "+ user.provider);
-		}else{
-			console.log('user is logged out');
-		}
 
-});
-	auth.login('google',{debug:'true'});
-});
+  //console.log('abhijeet');	
+//	auth.login('google',{debug:'true'});
 
