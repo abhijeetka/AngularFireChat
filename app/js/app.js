41bb5eded1084122b23390d408c93f1655b1b635
abhijeetka <abhijeet.kamble619@gmail.com>
2014-03-28 19:40:39 +0530

    upated

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-// Declare app level module which depends on filters, and services
+/* Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
   'myApp.filters',
@@ -13,4 +13,21 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]);*/
+
+var djWadiApp = angular.module('djWadiApp',['dependencyControllers','ngRoute','firebase']);
+
+djWadiApp.config(['$routeProvider',function($routeProvider){
+	$routeProvider
+	.when('/login',{
+		templateUrl:'partials/login.html',
+		controller:'controller1'
+		
+	})
+	.when('/welcome',{
+		templateUrl: 'partials/welcome.html',
+		controller: 'controller1'
+	})
+	.otherwise({redirectTo: '/login'});
+
 }]);
